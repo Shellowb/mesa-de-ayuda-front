e39e91891a0970778e1595b716db99c6694e2296
Pablo Arancibia Barahona <pabloarancibiabarahona@MacBook-Pro-de-Pablo.local>
2020-10-10 21:23:19 -0300

    Updates

---
+++ precache-manifest.230dc82c9bb26861d26fbf1878585f18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c73a4ad901c7585e7b0728af4ff8378",
+    "url": "/mesa-de-ayuda-front/index.html"
+  },
+  {
+    "revision": "3ddb1b79c3f189b41a3d",
+    "url": "/mesa-de-ayuda-front/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "947b23c985d48d193801",
+    "url": "/mesa-de-ayuda-front/static/css/main.4fb6ae2b.chunk.css"
+  },
+  {
+    "revision": "3ddb1b79c3f189b41a3d",
+    "url": "/mesa-de-ayuda-front/static/js/2.ab7b6eee.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/mesa-de-ayuda-front/static/js/2.ab7b6eee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "947b23c985d48d193801",
+    "url": "/mesa-de-ayuda-front/static/js/main.8d292f7f.chunk.js"
+  },
+  {
+    "revision": "a1a72ceb51057563c932",
+    "url": "/mesa-de-ayuda-front/static/js/runtime-main.f7a68fe3.js"
+  },
+  {
+    "revision": "ca98398c714e020b94ef49356314e7d2",
+    "url": "/mesa-de-ayuda-front/static/media/cool-background.ca98398c.png"
+  }
+]);
